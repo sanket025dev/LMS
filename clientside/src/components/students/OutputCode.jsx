b0d5c025ef4e11d556f6e72a8e0df08545ae80fe
sanket025dev <sanket025dev@gmail.com>
2025-05-13 00:58:42 +0530

    new commit

--- clientside/src/components/students/OutputCode.jsx
+++ clientside/src/components/students/OutputCode.jsx
@@ -1,31 +1,30 @@
 import React, {useState} from 'react'
-import { executeCode } from './api';
+// import { executeCode } from './api';
 
-const OutputCode = ({editorRef, fileName}) => {
+const OutputCode = () => {
+// {editorRef, fileName}
+  // const [output, setOutput] = useState(null)
 
-  const [output, setOutput] = useState(null)
-
-  const runCode = async() => {
-      const sourceCode = editorRef.current.getValue();
-      if (!sourceCode) return;
-      try {
-        const {run:result} = await executeCode(fileName,sourceCode)
-        setOutput(result.output)
-      }catch (error){}
-  };
+  // const runCode = async() => {
+  //     const sourceCode = editorRef.current.getValue();
+  //     if (!sourceCode) return;
+  //     try {
+  //       const {run:result} = await executeCode(fileName,sourceCode)
+  //       setOutput(result.output)
+  //     }catch (error){ }
+  // };
 
   return (
     <>
       <div className="h-75 w-1/2 bg-gray-800 flex flex-col">
         <h3 className='text-xl bg-white pt-3'>Output</h3>
         <div className='bg-white py-3'>
-          <button onClick={runCode} className= 'border border-orange-500 text-orange-500 py-2 px-4 mr-5 rounded hover:underline'>Run Code</button>
+          {/* onClick={runCode} */}
+          <button  className= 'border border-orange-500 text-orange-500 py-2 px-4 mr-5 rounded hover:underline'>Run Code</button>
         </div>
         <div className='text-gray-400 px-2'>
             <p>
-              {
-                output ? output : 'Click Run Code for the output'
-              }
+              Click Run Code for the output
             </p>
         </div>
       </div>
--- clientside/src/components/students/api.jsx
+++ clientside/src/components/students/api.jsx
@@ -1,17 +1,17 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-const API = axios.create({
-    baseURL: "https://emkc.org/api/v2/piston"
-})
+// const API = axios.create({
+//     baseURL: "https://emkc.org/api/v2/piston"
+// })
 
-export const executeCode = async (fileName, sourceCode) => {
+// export const executeCode = async (fileName, sourceCode) => {
 
-    const response = await API.post('/execute',{
-        "language": fileName,
-        "files": [
-          {
-            "content": sourceCode
-          },],
-        });
-        return response.data;
-}
+//     const response = await API.post('/execute',{
+//         "language": fileName,
+//         "files": [
+//           {
+//             "content": sourceCode
+//           },],
+//         });
+//         return response.data;
+// }
