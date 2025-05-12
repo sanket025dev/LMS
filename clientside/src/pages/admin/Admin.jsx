6e1f4ba2aec3ecdc3c7c33414dc2c918447106ba
sanket025dev <sanket025dev@gmail.com>
2025-05-12 22:13:32 +0530

    new commit

--- clientside/src/pages/admin/Admin.jsx
+++ clientside/src/pages/admin/Admin.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Footer from '../../components/teachers/Footer'
-import Sidebar from '../../components/admin/sidebar'
+import Sidebar from '../../components/admin/Sidebar'
 
 const Admin = () => {
   return (
